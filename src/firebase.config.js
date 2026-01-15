c635ba12bae0c779c522266597465b6f1cf01833
Tu Nombre <hernanenriquefernandez2019@gmail.com>
2026-01-14 23:07:18 -0300

    Fix: ocultando credenciales reales en firebase config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,16 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCwXvjVlys3Q0bdEqgBSVwaGfwRl4tUwY4",
-  authDomain: "lavarap-18ef0.firebaseapp.com",
-  projectId: "lavarap-18ef0",
-  storageBucket: "lavarap-18ef0.firebasestorage.app",
-  messagingSenderId: "509887694211",
-  appId: "1:509887694211:web:3a30f06dfd2d12e548a2b6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Inicializa primero
 const app = initializeApp(firebaseConfig);
-
-// Luego exporta la base de datos
 export const db = getFirestore(app);
